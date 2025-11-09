a0bc96da7c7b7ffe72067c2f069134283262d744
kisus <kisusmister@gmail.com>
2025-11-10 02:51:28 +0300

    KS-1-2 add solution

---
+++ app/src/main/kotlin/Lesson_1/Lesson1_task2.kt
@@ -0,0 +1,14 @@
+package org.example.Lesson_1
+
+fun main() {
+    var orderCount: Int = 75
+    val successPurchaseText: String = "Thanks for the order!"
+
+    var employeeCount: Int = 2000
+
+    println(orderCount)
+    println(successPurchaseText)
+
+    // println(employeeCount)
+    println(--employeeCount)
+}
