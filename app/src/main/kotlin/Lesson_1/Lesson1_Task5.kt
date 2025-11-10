e3cc4211dc79d992cea613b22d3985badd3dd020
kisus <kisusmister@gmail.com>
2025-11-11 01:42:05 +0300

    KS-1-5 убрал использование toInt()

--- app/src/main/kotlin/Lesson_1/Lesson1_Task5.kt
+++ app/src/main/kotlin/Lesson_1/Lesson1_Task5.kt
@@ -3,10 +3,10 @@ package org.example.Lesson_1
 const val TIME_DIVIDER = 60
 
 fun main() {
-    var seconds = "6480"
-    val hours = String.format("%02d", seconds.toInt() / TIME_DIVIDER / TIME_DIVIDER)
-    val minutes = String.format("%02d", seconds.toInt() / TIME_DIVIDER % TIME_DIVIDER)
-    seconds = String.format("%02d", seconds.toInt() % TIME_DIVIDER)
+    var seconds = 6480
+    val hours = seconds / TIME_DIVIDER / TIME_DIVIDER
+    val minutes = seconds / TIME_DIVIDER % TIME_DIVIDER
+    seconds = seconds % TIME_DIVIDER
 
-    println("$hours:$minutes:$seconds")
+    println("%02d:%02d:%02d".format(hours, minutes, seconds))
 }
