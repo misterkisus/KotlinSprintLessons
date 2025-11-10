30254f65e23fcb73a0710a8c8d7f8afac9bf4dc8
kisus <kisusmister@gmail.com>
2025-11-10 14:22:21 +0300

    KS-1-5 внес правки по ревью

--- app/src/main/kotlin/Lesson_1/Lesson1_Task5.kt
+++ app/src/main/kotlin/Lesson_1/Lesson1_Task5.kt
@@ -1,13 +1,12 @@
 package org.example.Lesson_1
 
+const val TIME_DIVIDER = 60
+
 fun main() {
-    var seconds = 6480
-    var hours = 0
-    var minutes = 0
+    var seconds = "6480"
+    val hours = String.format("%02d", seconds.toInt() / TIME_DIVIDER / TIME_DIVIDER)
+    val minutes = String.format("%02d", seconds.toInt() / TIME_DIVIDER % TIME_DIVIDER)
+    seconds = String.format("%02d", seconds.toInt() % TIME_DIVIDER)
 
-    hours = seconds / 60 / 60
-    minutes = seconds / 60 % 60
-    seconds = seconds % 60
-
-    println("0$hours:$minutes:0$seconds")
+    println("$hours:$minutes:$seconds")
 }
