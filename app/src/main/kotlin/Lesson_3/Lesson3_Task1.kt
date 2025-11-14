7d6eafd3f51c255befe64a5140d844fd689fad4d
kisus <kisusmister@gmail.com>
2025-11-14 13:55:28 +0300

    KS-3-1 добавил решение

---
+++ app/src/main/kotlin/Lesson_3/Lesson3_Task1.kt
@@ -0,0 +1,13 @@
+package org.example.Lesson_3
+
+fun main() {
+    val username = readln()
+
+    var greeting = "Добрый день, $username!"
+    println(greeting)
+
+
+    greeting = "Добрый вечер, $username!"
+    println(greeting)
+
+}
