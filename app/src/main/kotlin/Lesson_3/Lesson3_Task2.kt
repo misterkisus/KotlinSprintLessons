fa12ae804c1446d99156304393cf8b335bd1b3cd
kisus <kisusmister@gmail.com>
2025-11-14 14:09:39 +0300

    KS-3-2 добавил решение

---
+++ app/src/main/kotlin/Lesson_3/Lesson3_Task2.kt
@@ -0,0 +1,15 @@
+package org.example.Lesson_3
+
+fun main() {
+    val firstname = "Татьяна"
+    var surname = "Андреева"
+    val lastname = "Сергеевна"
+    var age = 20
+
+    println("$surname, $firstname, $lastname, $age")
+
+    surname = "Сидорова"
+    age = 22
+    println("$surname, $firstname, $lastname, $age")
+
+}
