8deccbdc98e738101bb5dc88fe0b6834e02a0f46
kisus <kisusmister@gmail.com>
2025-11-14 14:34:57 +0300

    KS-3-3 добавил решение

---
+++ app/src/main/kotlin/Lesson_3/Lesson3_Task3.kt
@@ -0,0 +1,17 @@
+package org.example.Lesson_3
+
+fun main() {
+    val number = 6
+
+    println("""
+        $number x 1 = ${number}
+        $number x 2 = ${number * 2}
+        $number x 3 = ${number * 3}
+        $number x 4 = ${number * 4}
+        $number x 5 = ${number * 5}
+        $number x 6 = ${number * 6}
+        $number x 7 = ${number * 7}
+        $number x 8 = ${number * 8}
+        $number x 9 = ${number * 9}
+    """.trimIndent())
+}
