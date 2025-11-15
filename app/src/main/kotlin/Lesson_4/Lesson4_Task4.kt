1d30149200ce9aa9c410887324435c4575fef373
kisus <kisusmister@gmail.com>
2025-11-15 03:02:25 +0300

    KS-4-4 убрал дублирование кода

--- app/src/main/kotlin/Lesson_4/Lesson4_Task4.kt
+++ app/src/main/kotlin/Lesson_4/Lesson4_Task4.kt
@@ -2,11 +2,12 @@ package org.example.Lesson_4
 
 fun main() {
     val currentDay = 5
+    val isCurrentDayEven = currentDay % 2 == 0
 
     println("""
-        Упражнения для рук: ${currentDay % 2 == 1}
-        Упражнения для ног: ${currentDay % 2 == 0}
-        Упражнения для спины: ${currentDay % 2 == 0}
-        Упражнения для пресса: ${currentDay % 2 == 1}
+        Упражнения для рук: ${!isCurrentDayEven}
+        Упражнения для ног: ${isCurrentDayEven}
+        Упражнения для спины: ${isCurrentDayEven}
+        Упражнения для пресса: ${!isCurrentDayEven}
     """.trimIndent())
 }
